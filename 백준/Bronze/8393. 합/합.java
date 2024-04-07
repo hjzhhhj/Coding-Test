fa74194e467102b660623cbce9fb9da7c40946b3
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-04-08 08:17:40 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String args[]) {
+public class Main{
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        int sum = 0;
+        int s = 0;
         for(int i=0; i <= n; i++){
-            sum += i;
+            s += i;
         }
-            System.out.println(sum);
+            System.out.println(s);
         
     }
 
