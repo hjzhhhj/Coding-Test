79ca0d675b453fdd81d90bf487eaa2fa2c3cff19
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-05-20 23:52:42 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,7 +5,7 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int s = 0;
-        for(int i=0; i <= n; i++){
+        for(int i = 0; i <= n; i++){
             s += i;
         }
             System.out.println(s);
