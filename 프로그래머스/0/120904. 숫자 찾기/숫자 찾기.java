60eac57c4532c2effb7cc96e41aaf4aceaf60e70
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-09-24 23:58:07 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num, int k) {
+        String s = Integer.toString(num);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) - '0' == k) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
