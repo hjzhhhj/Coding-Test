fb14b335bc895b1a5f9b7a7fcdd95c3e217500fa
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-09-26 18:53:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int hi = 1;
+        for ( int i = 1; i < 362880; i++ ) {
+            hi *= i;
+            if ( n / hi < 1 ) {
+                answer = i-1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
