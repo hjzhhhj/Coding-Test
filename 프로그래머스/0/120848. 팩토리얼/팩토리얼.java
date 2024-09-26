40fbf2b4d8a4b6e5920893742662f4c4c6bfa1d8
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-09-26 18:57:05 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         int hi = 1;
-        for ( int i = 1; i < 362880; i++ ) {
+        for ( int i = 1; i < 20; i++ ) {
             hi *= i;
             if ( n / hi < 1 ) {
                 answer = i-1;
