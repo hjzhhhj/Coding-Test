8efcd7a69eeca8c473ef55e0d69d35ad103ff690
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-10-14 23:02:07 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int answer2 = 0;
+        for ( int i = 0; i < num_list.length; i++ ) {
+            if ( i % 2 == 0 ) {
+                answer += num_list[i];
+            }
+            else if ( i % 2 != 0 ) {
+                answer2 += num_list[i];
+            }
+        }
+        if ( answer < answer2 ) {
+            answer = answer2;
+        }
+        return answer;
+    }
+}
