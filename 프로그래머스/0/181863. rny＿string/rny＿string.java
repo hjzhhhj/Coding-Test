94c0d4da5874538ea42f92ef835310750031b41b
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-10-17 23:08:12 +0900

    [level 0] Title: rny_string, Time: 14.98 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        for ( int i = 0; i < rny_string.length(); i++ ) {
+            char ch = rny_string.charAt(i);
+            if ( ch == 'm' ) {
+                answer += "rn";
+            }
+            else {
+                answer += ch;
+            }
+        }
+        return answer;
+    }
+}
