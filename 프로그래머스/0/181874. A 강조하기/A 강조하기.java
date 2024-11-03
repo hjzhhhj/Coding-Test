425b54edd32cdc84e4b9abfefaba6d8d7d789fdb
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-11-03 21:57:32 +0900

    [level 0] Title: A 강조하기, Time: 10.95 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        myString = myString.toLowerCase();
+        for ( int i = 0; i < myString.length(); i++ ) {
+            char ch = myString.charAt(i);
+            if ( ch == 'a' || ch == 'A' ) {
+                answer += "A";
+            }
+            else {
+                answer += ch;
+            }
+        }
+        return answer;
+    }
+}
