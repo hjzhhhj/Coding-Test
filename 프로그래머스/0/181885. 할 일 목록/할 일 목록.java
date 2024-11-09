23e32e7fc80320294c4cbec67072f53f9f8485c6
희진 <165463632+hjzhhhj@users.noreply.github.com>
2024-11-09 23:58:19 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        int a = 0;
+        for ( int i = 0; i < todo_list.length; i++ ) {
+            if ( finished[i] == false ) {
+                a++;
+            }
+        }
+        String[] answer = new String[a];
+        int b = 0;
+        for ( int i = 0; i < todo_list.length; i++ ) {
+            if ( finished[i] == false ) {
+                answer[b] = todo_list[i];
+                b++;
+            }
+        }
+        return answer;
+    }
+}
